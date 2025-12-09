c16d455b0ccd310181c0bf7f3b65e7dadb8d6033
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-09 17:29:40 +0100

    cefhelper: task: CloseTask: skip extra intermediate task object
    CefBrowserHost::CloseBrowser() automatically dispatching to the
    UI thread if necessary, so we don't need to do it anymore.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- cefhelper/task/CloseTask.h
+++ cefhelper/task/CloseTask.h
@@ -1,22 +1,8 @@
 
 #include "../cefhelper_priv.h"
 
-class CloseTask: public CefTask {
-public:
-    CloseTask(CefBrowserRef b) : browser(b) {}
-    void Execute() override {
-        if (!browser)
-            return;
-        if (!browser->GetHost())
-            return;
-        browser->GetHost()->CloseBrowser(false);
-    }
-
-private:
-    CefBrowserRef browser;
-    IMPLEMENT_REFCOUNTING(CloseTask);
-};
-
 void taskClose(CefBrowserRef b) {
-    CefPostTask(TID_UI, new CloseTask(b));
+    // already dispatching itself to UI thread
+    if (b && b->GetHost())
+        b->GetHost()->CloseBrowser(false);
 }
