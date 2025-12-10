ad9d6bd79a6efd0e2bdfd92e5fac4ed4b50867fc
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-10 14:45:06 +0100

    cefhelper: task: ExecuteScriptTask: skip extra intermediate task object
    CefFrame::ExecuteJavaScript() chain automatically dispatching to the
    UI thread if necessary, so we don't need to do it anymore.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- cefhelper/task/ExecuteScriptTask.h
+++ cefhelper/task/ExecuteScriptTask.h
@@ -1,22 +1,7 @@
 
 #include "../cefhelper_priv.h"
 
-class ExecuteScriptTask : public CefTask {
-public:
-    ExecuteScriptTask(CefBrowserRef b, const std::string& code)
-        : browser(b), script(code) {}
-    void Execute() override {
-        if (!browser || !browser->GetMainFrame())
-            return;
-        browser->GetMainFrame()->ExecuteJavaScript(script, "about:blank", 0);
-    }
-
-private:
-    CefBrowserRef browser;
-    std::string script;
-    IMPLEMENT_REFCOUNTING(ExecuteScriptTask);
-};
-
 void taskExecuteScript(CefBrowserRef b, const std::string& code) {
-    CefPostTask(TID_UI, new ExecuteScriptTask(b, code));
+    if (b && b->GetMainFrame())
+        b->GetMainFrame()->ExecuteJavaScript(code, "about:blank", 0);
 }
