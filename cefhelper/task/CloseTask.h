2605edd21c8d714bda5090725a7fe09769feaf75
Enrico Weigelt, metux IT consult <info@metux.net>
2026-01-13 13:17:38 +0100

    1

--- cefhelper/task/CloseTask.h
+++ cefhelper/task/CloseTask.h
@@ -3,6 +3,17 @@
 
 void taskClose(CefBrowserRef b) {
     // already dispatching itself to UI thread
-    if (b && b->GetHost())
-        b->GetHost()->CloseBrowser(false);
+    if (b) {
+        fprintf(stderr, "taskClose() browser ptr != null\n");
+        if (b->GetHost())
+            fprintf(stderr, "taskClose() GetHost() returns != null\n");
+        else
+            fprintf(stderr, "taskClose() GetHost() returns NULL\n");
+    } else
+        fprintf(stderr, "taskCloes() browser ptr == NULL\n");
+
+    if (b && b->GetHost()) {
+        fprintf(stderr, "taskClose() calling b->GetHost()->CloseBrowser()\n");
+        b->GetHost()->CloseBrowser(true);
+    }
 }
